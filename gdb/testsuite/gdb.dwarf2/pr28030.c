53478052c7da671a4f9bafa30188d1d40f9e2b0c
Alexandra Hájková <ahajkova@redhat.com>
2021-07-29 13:18:49 +0200

    Add the test case for pr28030

---
+++ gdb/testsuite/gdb.dwarf2/pr28030.c
@@ -0,0 +1,37 @@
+/* Copyright (C) 2018-2021 Free Software Foundation, Inc.
+
+   This file is part of GDB.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+typedef struct A {
+    int a;
+    int x;
+} A;
+
+struct A g_A;
+
+typedef struct B {
+    int a;
+    int x;
+} B;
+
+struct A g_B;
+
+int
+main (void)
+{
+  asm ("main_label: .globl main_label");
+  return 0;
+}
